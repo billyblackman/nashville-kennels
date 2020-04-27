de6d833135138a3cc5d42f0c47407a7682593842
William Daniel Blackman <blackmanbilly@gmail.com>
2020-04-27 14:53:56 -0500

    Fixed search bug

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import "./Kennel.css"
 import { LocationProvider } from "./location/LocationProvider"
 import LocationList from "./location/LocationList"
@@ -8,35 +8,48 @@ import { CustomerProvider } from "./customer/CustomerProvider"
 import CustomerList from "./customer/CustomerList"
 import { EmployeeProvider } from "./employee/EmployeeProvider"
 import EmployeeList from "./employee/EmployeeList"
+import { SearchBar } from "./search/SearchBar"
+import { SearchResults } from "./search/SearchResults"
 
-export default () => (
-    <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
 
-        <h2>Locations</h2>
-        <LocationProvider>
-            <LocationList />
-        </LocationProvider>
+export const Dashboard = () => {
+    const [searchTerms, setTerms] = useState("")
+    
+    return (
+            <>
+                <h2>Nashville Kennels</h2>
+                <small>Loving care when you're not there.</small>
 
-        <h2>Animals</h2>
-        <AnimalProvider>
-            <LocationProvider>
+                <h2>Locations</h2>
+                <LocationProvider>
+                    <LocationList />
+                </LocationProvider>
+
+                <h2>Animals</h2>
+                <AnimalProvider>
+                    <LocationProvider>
+                        <CustomerProvider>
+
+                        <div className="searchContainer">
+                            <SearchBar setTerms={setTerms} />
+                            <SearchResults searchTerms={searchTerms} />
+                        </div>
+
+                            <AnimalList />  
+                        </CustomerProvider>
+                    </LocationProvider>
+                </AnimalProvider>
+
+                <h2>Customers</h2>
                 <CustomerProvider>
-                    <AnimalList />  
+                    <CustomerList />
                 </CustomerProvider>
-            </LocationProvider>
-        </AnimalProvider>
 
-        <h2>Customers</h2>
-        <CustomerProvider>
-            <CustomerList />
-        </CustomerProvider>
-
-        <EmployeeProvider>
-            <LocationProvider>
-                <EmployeeList />
-            </LocationProvider>
-        </EmployeeProvider>
-    </>
-)
+                <EmployeeProvider>
+                    <LocationProvider>
+                        <EmployeeList />
+                    </LocationProvider>
+                </EmployeeProvider>
+            </>
+        )
+}
