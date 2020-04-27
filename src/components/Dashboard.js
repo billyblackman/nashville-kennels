f0b44dae3c0d353b5c13627e5d691d55c094b173
William Daniel Blackman <blackmanbilly@gmail.com>
2020-04-27 14:28:11 -0500

    Search bar now shows animals that match search terms

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import "./Kennel.css"
 import { LocationProvider } from "./location/LocationProvider"
 import LocationList from "./location/LocationList"
@@ -8,35 +8,48 @@ import { CustomerProvider } from "./customer/CustomerProvider"
 import CustomerList from "./customer/CustomerList"
 import { EmployeeProvider } from "./employee/EmployeeProvider"
 import EmployeeList from "./employee/EmployeeList"
+import { SearchBar } from "./search/SearchBar"
+import { SearchResults } from "./search/SearchResults"
 
-export default () => (
-    <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
+export const Dashboard = () => {
 
-        <h2>Locations</h2>
-        <LocationProvider>
-            <LocationList />
-        </LocationProvider>
+    const [searchTerms, setTerms] = useState("")
 
-        <h2>Animals</h2>
-        <AnimalProvider>
+    return (
+        <>
+            <h2>Nashville Kennels</h2>
+            <small>Loving care when you're not there.</small>
+
+            <h2>Locations</h2>
             <LocationProvider>
-                <CustomerProvider>
-                    <AnimalList />  
-                </CustomerProvider>
+                <LocationList />
             </LocationProvider>
-        </AnimalProvider>
 
-        <h2>Customers</h2>
-        <CustomerProvider>
-            <CustomerList />
-        </CustomerProvider>
+            <h2>Animals</h2>
+            <AnimalProvider>
+                <LocationProvider>
+                    <CustomerProvider>
 
-        <EmployeeProvider>
-            <LocationProvider>
-                <EmployeeList />
-            </LocationProvider>
-        </EmployeeProvider>
-    </>
-)
+                    <div className="searchContainer">
+                        <SearchBar setTerms={setTerms} />
+                        <SearchResults searchTerms={searchTerms} />
+                    </div>
+
+                        <AnimalList />  
+                    </CustomerProvider>
+                </LocationProvider>
+            </AnimalProvider>
+
+            <h2>Customers</h2>
+            <CustomerProvider>
+                <CustomerList />
+            </CustomerProvider>
+
+            <EmployeeProvider>
+                <LocationProvider>
+                    <EmployeeList />
+                </LocationProvider>
+            </EmployeeProvider>
+        </>
+    )
+}
